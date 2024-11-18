204455b62ffbdcdb71c9574ffd9922243a563d17
wo874 <92176181+wo874@users.noreply.github.com>
2024-11-17 22:42:32 -0500

    Update TempConverterApiApplication.kt

--- src/main/kotlin/seg3x02/tempconverterapi/TempConverterApiApplication.kt
+++ src/main/kotlin/seg3x02/tempconverterapi/TempConverterApiApplication.kt
@@ -1,11 +1,69 @@
 package seg3x02.tempconverterapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import org.springframework.context.annotation.Bean
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+
+import org.springframework.security.core.userdetails.User
+
+import org.springframework.security.web.SecurityFilterChain
+
+import org.springframework.security.core.userdetails.UserDetailsService
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+
+import org.springframework.security.crypto.password.PasswordEncoder
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
 import org.springframework.boot.runApplication
 
+
+
 @SpringBootApplication
-class TempConverterApiApplication
+class TempConverterApiApplicationclass TemperatureConversionApplication {
+
+    @Bean
+	
+    fun getEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+
+    @Bean
+    fun setupUsers(encoder: PasswordEncoder): UserDetailsService {
+	    
+        val firstUser = User.withUsername("firstUser")
+            .password(encoder.encode("password1"))
+            .authorities(SimpleGrantedAuthority("ROLE_USER"))
+            .build()
+
+        val secondUser = User.withUsername("secondUser")
+            .password(encoder.encode("password2"))
+            .authorities(SimpleGrantedAuthority("ROLE_USER"))
+            .build()
+
+        return InMemoryUserDetailsManager(firstUser, secondUser)
+    }
+
+    @Bean
+    fun securityConfig(security: HttpSecurity): SecurityFilterChain {
+        security
+	    
+            .csrf().disable()
+            .authorizeRequests()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic()
+        return security.build()
+    }
+}
 
 fun main(args: Array<String>) {
 	runApplication<TempConverterApiApplication>(*args)
 }
+
